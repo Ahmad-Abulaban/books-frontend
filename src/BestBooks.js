03bcfd3139d1467a470ea00f4b714be01db4c719
Ahmad-Abulaban <eng.ahmad090@gmail.com>
2021-09-27 21:25:23 +0300

    Done lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './BestBooks.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./BestBooks.css";
+import Card from "react-bootstrap/Card";
+import { CardGroup } from "react-bootstrap";
+import axios from "axios";
+import { withAuth0 } from '@auth0/auth0-react';
+
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount = async () => {
+    let {user} = this.props.auth0;
+    console.log(user.email);
+    let link = `${process.env.REACT_APP_SERVER}/books?emailin=${user.email}`;
+    let BooksBackend = await axios.get(link);
+    this.setState({
+      books: BooksBackend.data
+    });
+    console.log(this.state.books);
+  };
+
   render() {
-    return(
+    return (
       <>
         <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
+        <p>This is a collection of my favorite books</p>
+        {this.state.books && (
+          <CardGroup>
+            {this.state.books.map((item) => {
+              return (
+                <Card>
+                  <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>{item.description}</Card.Text>
+                    <Card.Text>{item.status}</Card.Text>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </CardGroup>
+        )}
       </>
-    )
+    );
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
